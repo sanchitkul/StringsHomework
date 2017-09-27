4755bdd96bdb83b762bcdf2dae238884902e843c
Your Name <you@example.com>
2017-09-27 02:09:39 -0400

    Added print function

--- index.php
+++ index.php
@@ -1,5 +1,25 @@
 <?php
 
-echo "Hello World";
+$obj = new main();
+$text="Proff Keith Williams";
+$obj-> printthis($text);
+class main{
 
+public function __construct()
+{
+echo '<h1>IS601</h1><br>';
+}
+public function printthis($text)
+{
+echo '<h2>Web Systems Development</h2>';
+print($text);
+echo'<hr>';
+}
+
+
+
+
+
+
+}
 ?>

69b30431b9b45b8ba3f206de883e8e0f62ac7407
Your Name <you@example.com>
2017-09-27 16:43:21 -0400

    Added char count and repeat function

--- index.php
+++ index.php
@@ -6,6 +6,8 @@ $obj-> printthis($text);
 $text_1="Thursday Class is Web Systems Development";
 $obj-> str_word_count($text_1);
 $obj-> str_shuffle($text_1);
+$obj-> strlen($text_1);
+$obj-> str_repeat($text_1);
 class main{
 
 public function __construct()
@@ -21,16 +23,36 @@ echo'<hr>';
 public function str_word_count($text_1)
 {
 $output = str_word_count($text_1);
-echo"How many words in the sentence:$text_1";
-echo'output--->'.$output;
+echo"How many words in the sentence: $text_1";
+echo'<br>output--->'.$output;
 echo'<hr>';
 }
 public function str_shuffle($text_1)
 {
 $output2 = str_shuffle($text_1);
 echo"Shuffle the sentence:$text_1:<br>";
-echo'output--->'.$output2;
+echo'<br>output--->'.$output2;
 echo'<hr>';
 }
+public function strlen($text_1)
+{
+$output3 = strlen($text_1);
+echo"Total no. of characters in the sentence : $text_1";
+echo'<br>output--->'.$output3;
+echo'<hr>';
+}
+public function str_repeat($text_1)
+{
+$output4 = str_repeat($text_1,2);
+echo"Repeat the sentence Once:$text_1";
+echo'<br>output--->'.$output4;
+echo'<hr>';
+}
+
+
+
+
+
+
 }
 ?>
